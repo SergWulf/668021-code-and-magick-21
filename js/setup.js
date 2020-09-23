614a5125fca11c6f5a6be7c5059741466eaf3f2e
SergWulf <36727388+SergWulf@users.noreply.github.com>
2020-09-23 11:14:22 +0300

    Исправления в коде

--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,10 @@
 'use strict';
 
-// Ищем окно и отображем его(убираем класс hidden)
-const setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-// Ищем блок, в котором будем отображать волшебников
-const similarListElement = setup.querySelector('.setup-similar-list');
-
-// Создаем шаблон для отображения волшебника
-const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
 const COUNT_WIZARDS = 4;
-const vornames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-const listCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-const listEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+const NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+const LIST_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+const LIST_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 const randomElementOfArray = function (listElements) {
   return Math.floor(Math.random() * listElements.length);
@@ -23,10 +13,10 @@ const randomElementOfArray = function (listElements) {
 const createWizards = function (countWizards) {
   const wizards = [];
   for (let i = 0; i < countWizards; i++) {
-    let oldWizard = {
-      name: vornames[randomElementOfArray(vornames)] + ' ' + surnames[randomElementOfArray(surnames)],
-      coatColor: listCoatColor[randomElementOfArray(listCoatColor)],
-      eyesColor: listEyesColor[randomElementOfArray(listEyesColor)]
+    const oldWizard = {
+      name: NAMES[randomElementOfArray(NAMES)] + ' ' + SURNAMES[randomElementOfArray(SURNAMES)],
+      coatColor: LIST_COAT_COLOR[randomElementOfArray(LIST_COAT_COLOR)],
+      eyesColor: LIST_EYES_COLOR[randomElementOfArray(LIST_EYES_COLOR)]
     };
     wizards.push(oldWizard);
   }
@@ -45,11 +35,22 @@ const renderWizard = function (wizard) {
 // Создаем волшебников
 const listWizards = createWizards(COUNT_WIZARDS);
 
+// Создаем шаблон для отображения волшебника
+const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
 // Создаем фрагмент и наполняем его волшебниками
 const fragment = document.createDocumentFragment();
-for (let j = 0; j < listWizards.length; j++) {
-  fragment.appendChild(renderWizard(listWizards[j]));
+for (let i = 0; i < listWizards.length; i++) {
+  fragment.appendChild(renderWizard(listWizards[i]));
 }
+
+// Ищем окно и отображем его(убираем класс hidden)
+const setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+// Ищем блок, в котором будем отображать волшебников
+const similarListElement = setup.querySelector('.setup-similar-list');
+
 // Вставляем в разметку новые блоки с волшебниками из фрагмента
 similarListElement.appendChild(fragment);
 
