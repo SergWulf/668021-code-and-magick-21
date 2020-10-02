36ec7f9d59c5a5dcc081fc5770e1235058998b4e
SergWulf <36727388+SergWulf@users.noreply.github.com>
2020-10-02 12:16:16 +0300

    Изменение цветов персонажа и файерболла по клику, сохранение значений цветов в скрытые поля формы

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,7 @@ const NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кри�
 const SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 const LIST_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 const LIST_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+const LIST_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 const randomElementOfArray = function (listElements) {
   return Math.floor(Math.random() * listElements.length);
@@ -95,4 +96,27 @@ setupClose.addEventListener('keydown', function (evt) {
       setup.classList.add('hidden');
     }
   }
+});
+
+// Находим блоки глаз, мантии и файерболла в окне настроек
+const coatWizard = setup.querySelector('.wizard-coat');
+const eyesWizard = setup.querySelector('.wizard-eyes');
+const fireballWizard = setup.querySelector('.setup-fireball-wrap');
+
+coatWizard.addEventListener('click', function () {
+  const currentCoatColor = LIST_COAT_COLOR[randomElementOfArray(LIST_COAT_COLOR)];
+  document.getElementsByName('coat-color')[0].value = currentCoatColor;
+  coatWizard.style.fill = currentCoatColor;
+});
+
+fireballWizard.addEventListener('click', function () {
+  const currentFireballColor = LIST_FIREBALL_COLOR[randomElementOfArray(LIST_FIREBALL_COLOR)];
+  document.getElementsByName('fireball-color')[0].value = currentFireballColor;
+  fireballWizard.style.background = currentFireballColor;
+});
+
+eyesWizard.addEventListener('click', function () {
+  const currentEyesColor = LIST_EYES_COLOR[randomElementOfArray(LIST_EYES_COLOR)];
+  document.getElementsByName('eyes-color')[0].value = currentEyesColor;
+  eyesWizard.style.fill = currentEyesColor;
 });
